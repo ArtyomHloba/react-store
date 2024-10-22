41196d6b0ca78bc2585ca93ae438dc2dcc157a42
ArtyomHloba <artemgloba2@gmail.com>
2024-10-22 20:55:23 +0300

    Add new years

--- src/components/FilterByYear/index.jsx
+++ src/components/FilterByYear/index.jsx
@@ -26,7 +26,8 @@ const FilterByYear = ({ selectedYear, onYearChange, onMonthChange }) => {
         >
           <option value='2021'>2021</option>
           <option value='2022'>2022</option>
-          {/* Можно добавить больше лет */}
+          <option value='2023'>2023</option>
+          <option value='2024'>2024</option>
         </select>
       </div>
       <div className={styles.filterByYearMonths}>
